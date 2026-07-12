2a12b1745050f9f98c96d5a776d2386a0cfc992b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "api_management_gateways" {
-  description = "All api_management_gateway resources"
-  value       = azurerm_api_management_gateway.api_management_gateways
-}
 output "api_management_gateways_api_management_id" {
-  description = "List of api_management_id values across all api_management_gateways"
-  value       = [for k, v in azurerm_api_management_gateway.api_management_gateways : v.api_management_id]
+  description = "Map of api_management_id values across all api_management_gateways, keyed the same as var.api_management_gateways"
+  value       = { for k, v in azurerm_api_management_gateway.api_management_gateways : k => v.api_management_id }
 }
 output "api_management_gateways_description" {
-  description = "List of description values across all api_management_gateways"
-  value       = [for k, v in azurerm_api_management_gateway.api_management_gateways : v.description]
+  description = "Map of description values across all api_management_gateways, keyed the same as var.api_management_gateways"
+  value       = { for k, v in azurerm_api_management_gateway.api_management_gateways : k => v.description }
 }
 output "api_management_gateways_location_data" {
-  description = "List of location_data values across all api_management_gateways"
-  value       = [for k, v in azurerm_api_management_gateway.api_management_gateways : v.location_data]
+  description = "Map of location_data values across all api_management_gateways, keyed the same as var.api_management_gateways"
+  value       = { for k, v in azurerm_api_management_gateway.api_management_gateways : k => v.location_data }
 }
 output "api_management_gateways_name" {
-  description = "List of name values across all api_management_gateways"
-  value       = [for k, v in azurerm_api_management_gateway.api_management_gateways : v.name]
+  description = "Map of name values across all api_management_gateways, keyed the same as var.api_management_gateways"
+  value       = { for k, v in azurerm_api_management_gateway.api_management_gateways : k => v.name }
 }
 
